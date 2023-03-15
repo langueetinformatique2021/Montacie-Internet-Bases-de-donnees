f87687548925d7b1d07d0fdf5e887ec8a68639cc
langueetinformatique2021 <langueetinformatique2021@gmail.com>
2023-03-15 11:08:17 +0100

    cours 6

---
+++ cours06/test3LectureMessage.java
@@ -0,0 +1,11 @@
+package cours06;
+
+public class test3LectureMessage {
+
+	public static void main(String args[]) {
+		LectureMessage p = new LectureMessage("pop.laposte.net", 110);
+		p.détruire(1, "ilgii1", "milgii1");
+		p.fin();
+	}
+
+}
